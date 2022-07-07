2927c61884cef6209d9fe853793c160f5ec8160d
Chonsawat Nakanam <chonsawat.nakanam@kkumail.com>
2022-07-07 12:22:52 +0000

    create run_densenet121_RGB.py & create run_efficientnetb0_RGB.py

---
+++ run_densenet121_RGB.py
@@ -0,0 +1,117 @@
+""" Documations
+Path:
+    Elab Dataset Path: 
+        input_path = "~/ChestXray-14/dataset/ChestXray NIH"
+"""
+# Modules
+from sklearn.utils import shuffle
+from tqdm.notebook import tqdm
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import os
+
+# ChestXray Required Modules
+from utils import *
+
+# Weight & Bias
+import wandb
+from wandb.keras import WandbCallback
+
+# Close warnings messages
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# ========= Check GPU ==============
+print('Using tensorflow %s' % tf.__version__)
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.config.list_physical_devices('GPU')))
+else:
+    print("Please install GPU version of TF")
+print("\n")
+
+# ============ Function =============
+def get_densenet121_model():
+    """
+    get_densenet121_model():
+        create the tensorflow model
+        ===========================
+        return:
+            pretrained-model restnet50
+            
+            
+        Example use
+        ===========
+        >>> model = get_densenet121_model()
+        
+    """
+    model = tf.keras.models.Sequential([
+        tf.keras.applications.densenet.DenseNet121(
+            include_top=False, 
+            input_shape=(None, None, 3),  # new dataset is grey-scale image
+            weights=None,
+            pooling='avg'
+        ),
+        tf.keras.layers.Dense(15, activation='sigmoid')  # 15 Output for new datasets
+    ])
+    return model
+
+
+# =========== Declare Variable ==============
+STRATEGY = tf.distribute.get_strategy()    
+BATCH_SIZE = 16
+IMG_SIZE = 224
+SEED = 42
+input_path = "dataset/ChestXray NIH"
+
+
+# ============ Main Program ======================================
+if tf.test.gpu_device_name():
+    """
+    Check if a GPU is none it's will terminate programs.
+    """
+    
+    # =========== Weight & Bias ==============
+    run = wandb.init(project="ChestXray",
+                    config = {
+                      "epochs": 20,
+                      "batch_size": BATCH_SIZE,
+                      "loss_function": "binary_crossentropy"
+                    })
+    config = wandb.config
+
+    
+    train_filenames = tf.io.gfile.glob(f'{input_path}/data/224x224/train/*.tfrec')
+    val_filenames = tf.io.gfile.glob(f'{input_path}/data/224x224/valid/*.tfrec')
+    test_filenames = tf.io.gfile.glob(f'{input_path}/data/224x224/test/*.tfrec')
+
+    # steps_per_epoch = count_data_items(train_filenames) // BATCH_SIZE
+    # validation_steps = count_data_items(val_filenames) // BATCH_SIZE
+
+    train_dataset = get_dataset(train_filenames, shuffled=False, repeated=False, augmented=False, color=True)
+    val_dataset = get_dataset(val_filenames, cached=True, color=True)
+
+    with STRATEGY.scope():
+        tf.keras.backend.clear_session()
+        model = get_densenet121_model()
+        
+        model.compile(
+            optimizer='adam',
+            loss=config.loss_function,
+            metrics=tf.keras.metrics.AUC(multi_label=True))
+
+    history = model.fit(
+        train_dataset,
+        epochs=config.epochs,
+        # steps_per_epoch=steps_per_epoch,
+        validation_data=val_dataset,
+        # validation_steps=validation_steps,
+        verbose=1,
+        callbacks=[WandbCallback()])
+
+    model.save(f"/home/jovyan/ChestXray-14/results/models/Densenet121_RGB_epochs_{config.epochs}.h5")
+    print("Save Model")
+else:
+    print("\n===== Please, install GPU =====")
+# ====================================================================
---
+++ run_efficientnetb0_RGB.py
@@ -0,0 +1,95 @@
+# Modules
+from sklearn.utils import shuffle
+from tqdm.notebook import tqdm
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import os
+
+# ChestXray Required Modules
+from utils import *
+
+# Weight & Bias
+import wandb
+from wandb.keras import WandbCallback
+
+# Close warnings messages
+import warnings
+warnings.filterwarnings('ignore')
+
+# ========= Check GPU ==============
+print('Using tensorflow %s' % tf.__version__)
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.config.list_physical_devices('GPU')))
+else:
+    print("Please install GPU version of TF")
+print("\n")
+
+
+# ============ Function =============
+def get_model():
+    model = tf.keras.models.Sequential([
+        tf.keras.applications.EfficientNetB0(
+            include_top=False,
+            input_shape=(None, None, 3),
+            weights=None,
+            pooling='avg'),
+        tf.keras.layers.Dense(15, activation='sigmoid')
+    ])
+    return model
+
+# =========== Weight & Bias ==============
+run = wandb.init(project="ChestXray",
+                config = {
+                  "epochs": 20,
+                  "batch_size": BATCH_SIZE,
+                  "loss_function": "binary_crossentropy"
+                })
+config = wandb.config
+
+
+# =========== Declare Variable ==============
+STRATEGY = tf.distribute.get_strategy()    
+BATCH_SIZE = 16
+IMG_SIZE = 224
+SEED = 42
+input_path = "dataset/ChestXray NIH"
+
+
+# ============ Main Program ======================================
+if tf.test.gpu_device_name():
+    """
+    Check if a GPU is none it's will terminate programs.
+    """
+    train_filenames = tf.io.gfile.glob(f'{input_path}/data/224x224/train/*.tfrec')
+    val_filenames = tf.io.gfile.glob(f'{input_path}/data/224x224/valid/*.tfrec')
+    test_filenames = tf.io.gfile.glob(f'{input_path}/data/224x224/test/*.tfrec')
+
+    # steps_per_epoch = count_data_items(train_filenames) // BATCH_SIZE
+    # validation_steps = count_data_items(val_filenames) // BATCH_SIZE
+
+    train_dataset = get_dataset(train_filenames, shuffled=False, repeated=False, augmented=False)
+    val_dataset = get_dataset(val_filenames, cached=True)
+
+    with STRATEGY.scope():
+        tf.keras.backend.clear_session()
+        model = get_model()
+        
+        model.compile(
+            optimizer='adam',
+            loss=config.loss_function,
+            metrics=tf.keras.metrics.AUC(multi_label=True))
+
+    history = model.fit(
+        train_dataset,
+        epochs=config.epochs,
+        # steps_per_epoch=steps_per_epoch,
+        validation_data=val_dataset,
+        # validation_steps=validation_steps,
+        verbose=1,
+        callbacks=[WandbCallback()])
+
+    model.save(f"/home/jovyan/ChestXray-14/results/models/EfficientNetB0_RGB_epochs-{config.epochs}.h5")
+else:
+    print("\n===== Please, install GPU =====")
+# ====================================================================
