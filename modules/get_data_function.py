8e4847e84355ea32496d571fff6a368bb6629c84
Chonsawat Nakanam <chonsawat.nakanam@kkumail.com>
2022-07-26 09:13:35 +0000

    Functional get dataset

--- modules/get_data_function.py
+++ modules/get_data_function.py
@@ -105,3 +105,30 @@ def get_model():
         metrics=tf.keras.metrics.AUC(multi_label=True))
 
     return model
+
+
+class DatasetX:
+    BATCH_SIZE = 16
+    IMG_SIZE = 224
+    SEED = 42
+    input_path = "dataset/ChestXray NIH"
+    
+    def get_train(self):
+        filenames = tf.io.gfile.glob(f'{self.input_path}/data/224x224/train/*.tfrec')
+        dataset = get_dataset(filenames)
+        return dataset
+
+    def get_test(self):
+        filenames = tf.io.gfile.glob(f'{self.input_path}/data/224x224/test/*.tfrec')
+        dataset = get_dataset(filenames)
+        return dataset
+
+    def get_valid(self):
+        filenames = tf.io.gfile.glob(f'{self.input_path}/data/224x224/valid/*.tfrec')
+        dataset = get_dataset(filenames)
+        return dataset
+    
+    def get_all(self):
+        filenames = tf.io.gfile.glob(f'{self.input_path}/data/224x224/All/*.tfrec')
+        dataset = get_dataset(filenames)
+        return dataset
