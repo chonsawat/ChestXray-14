5b4ad4506a29cb258b8a17f7de36176bba08c5aa
Chonsawat Nakanam <chonsawat.nakanam@kkumail.com>
2022-09-11 15:56:54 +0700

    fix DRY principles in multiple files

--- prepare_dataset_sample_5_folds.py
+++ prepare_dataset_sample_5_folds.py
@@ -4,55 +4,13 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 from modules.dataset import Directory
+from modules.utils import serialize_fold
 
 INPUT_PATH = "dataset/ChestXray NIH"
 STRATEGY = tf.distribute.get_strategy()    
 BATCH_SIZE = 16
 IMG_SIZE = 224
 SEED = 42
-
-def _serialize_image(path):
-    image = tf.io.read_file(path)
-    image = tf.image.decode_png(image, channels=1)
-    image = tf.image.resize(image, [IMG_SIZE, IMG_SIZE])
-    image = tf.cast(image, tf.uint8)
-    return tf.image.encode_jpeg(image).numpy()
-
-
-def _serialize_sample(image_id, image, proba):
-    feature = {
-        'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image])),
-        'image_id': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_id])),
-        'No Finding': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[0]])),
-        'Atelectasis': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[1]])),
-        'Consolidation': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[2]])),
-        'Infiltration': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[3]])),
-        'Pneumothorax': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[4]])),
-        'Edema': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[5]])),
-        'Emphysema': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[6]])),
-        'Fibrosis': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[7]])),
-        'Effusion': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[8]])),
-        'Pneumonia': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[9]])),
-        'Pleural_Thickening': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[10]])),
-        'Cardiomegaly': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[11]])),
-        'Nodule': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[12]])),
-        'Mass': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[13]])),
-        'Hernia': tf.train.Feature(int64_list=tf.train.Int64List(value=[proba[14]]))}
-    sample = tf.train.Example(features=tf.train.Features(feature=feature))
-    return sample.SerializeToString()
-
-
-def serialize_fold(fold, name):
-    samples = []
-    
-    for index, proba in fold.iterrows():
-        samples.append(_serialize_sample(
-            index.split('/')[-1].encode(), 
-            _serialize_image(index), 
-            proba))
-    
-    with tf.io.TFRecordWriter(name + '.tfrec') as writer:
-        [writer.write(x) for x in samples]
         
 df = pd.read_csv(f"{INPUT_PATH}/preprocessed_data.csv", index_col=0)
 df = df.astype("int16")
