5033e4af31563e78516d1827010de4bcfb6eabbe
Chonsawat Nakanam <chonsawat.nakanam@kkumail.com>
2023-04-08 14:24:59 +0000

    create training under sampling train valid test cross entropy and drouput

--- experiments/Under_sampling-multilabel_classification/modules/models.py
+++ experiments/Under_sampling-multilabel_classification/modules/models.py
@@ -194,3 +194,71 @@ class Model_with_dropout_freeze_model_3:
         self.create_model()
         self.compile_model()
         return self.model
+    
+    
+class Model_under_sampling_dropout:
+    """ Use for create a different transfer learning model
+    Example
+    --------
+    >>> from modules.models import Model
+    >>> transfer_model = tf.keras.applications.resnet50.ResNet50(
+        include_top=True, 
+        weights=None,
+        pooling='avg'
+    )
+    >>> model = Model(transfer_model).get_model()
+    """
+    def __init__(self, transfer_model):
+        self.optimizer = tf.keras.optimizers.Adam()
+        self.loss = tf.keras.losses.BinaryCrossentropy(from_logits=False)
+        self.metrics = [tf.keras.metrics.AUC(multi_label=True), tfa.metrics.F1Score(num_classes=15), tf.keras.metrics.Precision(), tf.keras.metrics.Recall()]
+        self.transfer = transfer_model
+        self.model = None
+        
+    def create_model(self):
+        """ Create a Sequential of model for instances
+        Example
+        -------
+        >>> self.create_model()
+        """        
+        sequential_list = [
+            self.transfer,
+            tf.keras.layers.Flatten(),
+            
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dropout(0.25),
+            
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dropout(0.25),
+            
+            tf.keras.layers.Dense(64, activation="relu"),
+            tf.keras.layers.Dropout(0.25),
+            
+            tf.keras.layers.Dense(64, activation="relu"),
+            tf.keras.layers.Dropout(0.25),
+            
+            tf.keras.layers.Dense(15, activation='sigmoid')
+        ]
+        self.model = tf.keras.Sequential(sequential_list)
+    
+    def compile_model(self):
+        """Compile a Sequential of model for instances
+        Example
+        -------
+        >>> self.compile_model()
+        """        
+        self.model.compile(optimizer=self.optimizer,
+                           loss=self.loss,
+                           metrics=self.metrics)
+        
+    def get_model(self):
+        """Create and Compile a Sequential of model for instances
+
+        Returns
+        -------
+        tf.Model
+            a Sequential of model
+        """
+        self.create_model()
+        self.compile_model()
+        return self.model
